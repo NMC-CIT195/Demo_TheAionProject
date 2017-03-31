3752f9fd9f6cd0b49b2af3d3c1e2aec9df273955
John Velis <john@johnvelis.com>
2017-03-31 12:16:04 -0400

    S4 initial

---
+++ TheAionProject.S4_Solution/Models/Civilian.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheAionProject.Models
+{
+    public class Civilian : Npc, ISpeak
+    {
+        public List<string> Messages { get; set; }
+
+        public string Speak()
+        {
+            if (this.Messages != null)
+            {
+                return GetMessage();
+            }
+            else
+            {
+                return $"My name is {base.Name} and I am a {base.Race}";
+            }
+        }
+
+        private string GetMessage()
+        {
+            Random r = new Random();
+            int messageIndex = r.Next(0, Messages.Count());
+            return Messages[messageIndex];
+        }
+    }
+}
---
+++ TheAionProject.S4_Solution/Models/Npc.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheAionProject.Models
+{
+    public class Npc : Character
+    {
+        public string Description { get; set; }
+    }
+}
